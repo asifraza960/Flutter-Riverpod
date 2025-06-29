1ccd56a1fc8eb3c9b2b2fcb3d837b820ec1550ec
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-06-29 13:55:55 +0500

    update

--- state_provider/lib/State_provider/counter_provider.dart
+++ state_provider/lib/State_provider/counter_provider.dart
@@ -1,9 +1,12 @@
 import 'package:riverpod/riverpod.dart';
 
 
-
-
-
 final  CounterProvider = StateProvider <int>((ref){
 return 0;
 });
+
+
+final switchprovider = StateProvider<bool>((ref){
+return false;
+}
+);
--- state_provider/lib/State_provider/home.dart
+++ state_provider/lib/State_provider/home.dart
@@ -7,31 +7,50 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context,WidgetRef ref) {
+    print("asi");
     return Scaffold(
       appBar: AppBar(
+      
         backgroundColor: Colors.blue,
+      
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Consumer(
               builder: (context ,ref , child){
                 final cont = ref.watch(CounterProvider);
                 print("build2");
                 return Center(
-                  child: Text(cont.toString()),
+                  child: Text(cont.toString(),style: TextStyle(
+                    fontSize: 23
+                  ),),
                 );
                 
               }
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(onPressed: (){
                     ref.read(CounterProvider.notifier).state++;
-                  }, child: Text("+"))
+                  }, child: Text("+",style: TextStyle(
+                    fontSize: 23
+                  ),))
                 ],
-              )
+              ),
               
+              Consumer(
+                builder: (context ,ref,child){
+                  print("3");
+                  final Switchp = ref.watch(switchprovider);
+                  return Switch(value: Switchp, onChanged: (value){
+                    ref.read(switchprovider.notifier).state = value;
+
+                  });
+                }
+                )
              
             
           ],
