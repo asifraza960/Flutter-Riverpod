dd78dac43c1fbb2631ef8eb4cbf5338b87d0323f
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-06-30 18:36:59 +0500

    update

---
+++ State_Notifier_Provider/lib/Provideres.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final SearchProvider = StateNotifierProvider<Searchstate,String>((ref){
+  return Searchstate();
+});
+
+
+class Searchstate extends StateNotifier<String>{
+  Searchstate () : super ('');
+
+  
+}
